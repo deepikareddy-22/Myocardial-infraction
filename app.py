4172674ab091503ef4289dadf04fe412614d2ce5
deepikareddy-22 <aluguvellideepikareddy22@gmail.com>
2025-03-28 00:10:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load the trained model
-with open("model.pkl", "rb") as f:
+with open("model_top10.pkl", "rb") as f:
     model = pickle.load(f)
 
 @app.route("/")
